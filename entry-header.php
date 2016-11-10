27360ad1cea1f39ea7b16961e72ecbbab5c22f56
cli-dev <websites.clidigital@gmail.com>
2016-11-10 13:47:17 -0700

    Fix for side nav

--- entry-header.php
+++ entry-header.php
@@ -4,21 +4,14 @@ $myoptions = get_option( 'themesettings_');
 $logo_position = $myoptions['logo_position'];
 $site_header_type = $myoptions['header_type'];
 $top_header_type = '';
-if ($site_header_type === 'Top Menu') { $top_header_type = $myoptions['top_header_position']; }
-$center_logo_menu_type = '';
-if($logo_position === 'center'){$center_logo_menu_type = $myoptions['center_logo_menu_type'];}
-
+$top_header_type = $myoptions['top_header_position'];
 $overlapping_header = '';
-
 if($top_header_type === "header-overlap") {
   $overlapping_header = ' overlapping-header';
 }
-
 $detect = new Mobile_Detect;
-
 $page_for_posts = get_option( 'page_for_posts' );
 $placeholder_img = get_field('background_image', $page_for_posts);
-
 $headerStyles = '';
 
 if($detect->isMobile() && has_post_thumbnail()) {
@@ -37,13 +30,15 @@ elseif(!$detect->isMobile() && has_post_thumbnail()){
   $headerStyles = 'style="background-image: url(' . $thumb_url . '); background-position: 50% 70%; background-repeat: no-repeat; background-attachment: fixed; background-size: cover;"';
 }
 else{
-  $headerStyles = 'style="background-image: url(' . $placeholder_img . '); background-position: 50% 70%; background-repeat: no-repeat; background-attachment: fixed; background-size: cover;"';
+  $headerStyles = 'style="background: url(' . $placeholder_img . ') center no-repeat; background-size: cover;"';
 }
 
 ?>
 
-<header class="post-header<?php echo $overlapping_header; ?>" <?php echo $headerStyles;?>><?php 
-  $title = get_the_title();
-  $attr = array('class' => "post-img-inner", 'alt' => $title,);
-  the_post_thumbnail( 'thumbnail', $attr );
-  ?></header>
+<header class="post-header<?php echo $overlapping_header; ?>" <?php echo $headerStyles;?>>
+  <?php 
+    $title = get_the_title();
+    $attr = array('class' => "post-img-inner", 'alt' => $title,);
+    the_post_thumbnail( 'thumbnail', $attr );
+  ?>
+</header>
