2b6b2a3abe29ed360498990f17409c92a473b249
cli-dev <websites.clidigital@gmail.com>
2017-02-03 12:09:45 -0700

    Blog Fixes

--- entry-content.php
+++ entry-content.php
@@ -1,9 +1,11 @@
-<header class="post-sub-header">
-  <h1 itemprop="name headline"><a href="<?php echo the_permalink(); ?>" class="post-title" itemprop="url"><?php the_title(); ?></a></h1>
-  <?php get_template_part( 'entry-meta' ); ?>
-  <?php get_template_part( 'entry-author' ); ?>
-</header>
+
 <section itemprop="articleBody" class="post-content">
+  <header class="post-title">
+    <h1 itemprop="name headline"><a href="<?php echo the_permalink(); ?>" itemprop="url"><?php the_title(); ?></a></h1>
+    <?php get_template_part( 'entry-meta' ); ?>
+    <?php get_template_part( 'entry-author' ); ?>
+  </header>
   <?php the_content(); ?>
+  <?php get_template_part( 'entry-footer' ); ?>
 </section>
-<?php get_template_part( 'entry-footer' ); ?>
+
